24ea5a462ecdcd8853e19b3774fe2cb630c4812d
Polyana Fucilini <poly.fucilini.s@gmail.com>
2022-07-13 23:04:19 -0300

    Response Entity

--- src/main/java/com/pixel/movies/controller/MovieController.java
+++ src/main/java/com/pixel/movies/controller/MovieController.java
@@ -3,6 +3,7 @@ package com.pixel.movies.controller;
 import com.pixel.movies.model.Movie;
 import com.pixel.movies.service.MovieService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -16,27 +17,27 @@ public class MovieController {
     private MovieService movieService;
 
     @PostMapping
-    public Movie createMovie(@RequestBody @Valid Movie movie) {
+    public ResponseEntity<String> createMovie(@RequestBody @Valid Movie movie) {
         return movieService.createMovie(movie);
     }
 
     @DeleteMapping(value = "/{id}")
-    public String deleteMovie(@PathVariable("id") final Long id) {
+    public ResponseEntity<String> deleteMovie(@PathVariable("id") final Long id) {
         return movieService.deleteMovie(id);
     }
 
     @GetMapping(value = "/{id}")
-    public Movie getMovieById(@PathVariable("id") final Long id) {
+    public ResponseEntity<Movie> getMovieById(@PathVariable("id") final Long id) {
         return movieService.getMovieById(id);
     }
 
     @GetMapping
-    public List<Movie> getAll() {
+    public ResponseEntity<List<Movie>>getAll() {
         return movieService.getAll();
     }
 
     @PutMapping(value = "/{id}")
-    public Movie updateMovieById(@RequestBody Movie movie, @PathVariable("id") final Long id) {
+    public ResponseEntity<String> updateMovieById(@RequestBody Movie movie, @PathVariable("id") final Long id) {
         return movieService.updateMovieById(movie, id);
     }
 }
--- src/main/java/com/pixel/movies/service/MovieService.java
+++ src/main/java/com/pixel/movies/service/MovieService.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import javax.validation.constraints.Null;
 import java.util.List;
 
 @Service
@@ -16,37 +17,58 @@ public class MovieService {
     @Autowired
     private MovieRepository movieRepository;
 
-    public Movie createMovie(Movie movie) {
-
-        return validateMovie(movie);
-    }
-
-    public String deleteMovie(Long id) {
-        movieRepository.deleteById(id);
-        return "Movie " + id + " was deleted";
-    }
-
-    public Movie getMovieById(Long id) {
-        return movieRepository.findById(id).get();
-    }
-
-    public List<Movie> getAll() {
-        return movieRepository.findAll();
-    }
-
-    public Movie updateMovieById(Movie movie, Long id) {
-        movie.setId(id);
-        return movieRepository.save(movie);
-    }
-
-    private Movie validateMovie(Movie movie) {
-        List<Movie> movies = getAll();
-        for (int i = 0; i < movies.size(); i++) {
-            if (movies.get(i).getTitle().equals(movie.getTitle())) {
-                throw new RuntimeException();
-            }
+    public ResponseEntity<String> createMovie(Movie movie) {
+        if (validateIfMovieExistsByTitle(movie)) {
+            return new ResponseEntity<>("This title already exists", HttpStatus.CONFLICT);
+        } else {
+            movieRepository.save(movie);
+            return new ResponseEntity<>("The movie was created", HttpStatus.CREATED);
         }
-        return movieRepository.save(movie);
+    }
+
+    public ResponseEntity<String> deleteMovie(Long id) {
+        if (movieRepository.existsById(id)) {
+            movieRepository.deleteById(id);
+            return new ResponseEntity<>("Movie " + id + " was deleted", HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("Id " + id + " was not found", HttpStatus.NOT_FOUND);
+        }
+    }
+
+    public ResponseEntity<Movie> getMovieById(Long id) {
+        if (movieRepository.existsById(id)) {
+            return new ResponseEntity<>(movieRepository.findById(id).get(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+    }
+
+    public ResponseEntity<List<Movie>> getAll() {
+        if (movieRepository.count() <= 0) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } else {
+            return new ResponseEntity<>(movieRepository.findAll(), HttpStatus.OK);
+        }
+
+    }
+
+    public ResponseEntity<String> updateMovieById(Movie movie, Long id) {
+        if (movieRepository.existsById(id)) {
+            if (validateIfMovieExistsByTitle(movie)) {
+                return new ResponseEntity<>("Movie already exists", HttpStatus.CONFLICT);
+            } else {
+                movie.setId(id);
+                movieRepository.save(movie);
+                return new ResponseEntity<>("Update was complete", HttpStatus.OK);
+            }
+        } else {
+            return new ResponseEntity<>("ID doesn't exist", HttpStatus.NOT_FOUND);
+        }
+    }
+
+    private Boolean validateIfMovieExistsByTitle(Movie movie) {
+        return movieRepository.existsByTitle(movie.getTitle());
     }
 }
 
